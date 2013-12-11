946eef2045cdb788565cc32b36ca76a4f69a871f
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2013-12-11 22:08:04 +0100

    on push des trucs qui marchent pas

--- src/traceroute.c
+++ src/traceroute.c
@@ -7,6 +7,12 @@
 #include "../include/traceroute.h"
 #include "../include/log.h"
 
+void Usage()
+{
+    printf("USAGE: traceroute servername\n");
+    exit(-1);
+}
+
 int SetTTL(Socket s, int ttl)
 {
     const int *ttl_p = &ttl;
@@ -20,32 +26,30 @@ int SetTTL(Socket s, int ttl)
     return 1;
 }
 
-void Usage()
+Socket OpenRawSocket()
 {
-    printf("USAGE: traceroute servername\n");
-    exit(-1);
-}
-
-Socket OpenRawSocket() {
-	Socket s;
-	if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) == -1) {
-		perror("Unable to open Raw Socket");
-		exit(1);
-	}
-	return s;
+    Socket s;
+    if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) == -1) {
+            perror("Unable to open Raw Socket");
+            exit(1);
+    }
+    return s;
 }
 
 /// IN DEV
-int ConstructIPHeader(struct iphdr* iph) {
-	iph->ihl = 5;
-	iph->version = 4;
-	iph->tos = 16; // Low delay
-	iph->id = htons(54321);
-	iph->ttl = ttl; // hops
-	iph->protocol = 17; // UDP
-	/* Source IP address, can be spoofed */
-	//~ iph->saddr = inet_addr(inet_ntoa(((struct sockaddr_in *)&if_ip.ifr_addr)->sin_addr));
-	iph->saddr = inet_addr("192.168.0.1");
-	/* Destination IP address */
-	iph->daddr = inet_addr("192.168.0.111");
+int ConstructIPHeader(struct iphdr* iph, const unsigned int ttl)
+{
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 16; // Low delay
+    iph->id = htons(54321);
+    iph->ttl = ttl; // hops
+    iph->protocol = 17; // UDP
+    /* Source IP address, can be spoofed */
+    //~ iph->saddr = inet_addr(inet_ntoa(((struct sockaddr_in *)&if_ip.ifr_addr)->sin_addr));
+    iph->saddr = inet_addr("192.168.0.1");
+    /* Destination IP address */
+    iph->daddr = inet_addr("192.168.0.111");
+    
+    return 0;
 }
