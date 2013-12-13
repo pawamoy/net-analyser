e8e0053ffd817983a9deff28f373dab982d07568
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2013-12-13 16:28:23 +0100

    wip traceroute

--- src/main_traceroute.c
+++ src/main_traceroute.c
@@ -6,6 +6,9 @@
 #include <assert.h>
 #include "../include/traceroute.h"
 #include "../include/log.h" 
+#include <errno.h>
+
+#define MAX_PACKET 1024
 
 /*
  * Transformer le nom de domaine fourni en adresse IP										DONE
@@ -29,9 +32,70 @@
  * http://pwet.fr/man/linux/conventions/packet
  * http://stackoverflow.com/questions/14774668/what-is-raw-socket-in-socket-programming
  * http://www.tenouk.com/Module43a.html
+ * http://forum.hardware.fr/hfr/Programmation/C-2/code-source-ping-sujet_30136_1.htm
+ * http://stackoverflow.com/questions/13543554/how-to-receive-icmp-request-in-c-with-raw-sockets
  */
+ 
+void SwitchErrno(int);
 
-
+void SwitchErrno(int err)
+{
+	switch (err)
+	{
+		case EACCES: printf("EACCES\n"); break;
+		 //~ (For UNIX domain sockets,  which  are  identified  by  pathname)
+		  //~ Write  permission  is  denied on the destination socket file, or
+		  //~ search permission is denied for one of the directories the  path
+		  //~ prefix.  (See path_resolution(7).)
+		  //~ (For  UDP  sockets)  An  attempt  was  made  to  send  to a net‐
+		  //~ work/broadcast address as though it was a unicast address.
+		case EAGAIN: printf("EAGAIN or EWOULDBLOCK\n"); break;
+		  //~ The socket is marked nonblocking  and  the  requested  operation
+		  //~ would  block.   POSIX.1-2001  allows either error to be returned
+		  //~ for this case, and does not require these constants to have  the
+		  //~ same value, so a portable application should check for both pos‐
+		  //~ sibilities.
+		case EBADF: printf("EBADF\n"); break;
+		//~ An invalid descriptor was specified.
+		case ECONNRESET: printf("ECONNRESET\n"); break;
+		  //~ Connection reset by peer.
+		case EDESTADDRREQ: printf("EDESTADDRREQ\n"); break;
+		  //~ The socket is not connection-mode, and no peer address is set.
+		case EFAULT: printf("EFAULT\n"); break;
+		//~ An invalid user space address was specified for an argument.
+		case EINTR: printf("EINTR\n"); break;
+			//~ A signal occurred before any  data  was  transmitted;  see  sig‐
+		  //~ nal(7).
+		case EINVAL:  printf("EINVAL\n"); break;
+		//~ Invalid argument passed.
+		case EISCONN: printf("EISCONN\n"); break;
+		  //~ The connection-mode socket was connected already but a recipient
+		  //~ was specified.  (Now either  this  error  is  returned,  or  the
+		  //~ recipient specification is ignored.)
+		case EMSGSIZE: printf("EMSGSIZE\n"); break;
+		  //~ The  socket  type  requires that message be sent atomically, and
+		  //~ the size of the message to be sent made this impossible.
+		case ENOBUFS: printf("ENOBUFS\n"); break;
+		  //~ The output queue for a network interface was full.  This  gener‐
+		  //~ ally  indicates  that the interface has stopped sending, but may
+		  //~ be caused by transient congestion.   (Normally,  this  does  not
+		  //~ occur in Linux.  Packets are just silently dropped when a device
+		  //~ queue overflows.)
+		case ENOMEM:  printf("ENOMEM\n"); break;
+		//~ No memory available.
+		case ENOTCONN: printf("ENOTCONN\n"); break;
+		  //~ The socket is not connected, and no target has been given.
+		case ENOTSOCK: printf("ENOTSOCK\n"); break;
+		  //~ The argument sockfd is not a socket.
+		case EOPNOTSUPP: printf("EOPNOTSUPP\n"); break;
+		  //~ Some bit in the flags argument is inappropriate for  the  socket
+		  //~ type.
+		case EPIPE:   printf("EPIPE\n"); break;
+		//~ The  local  end  has  been  shut  down  on a connection oriented
+		  //~ socket.  In this case the process will also  receive  a  SIGPIPE
+		  //~ unless MSG_NOSIGNAL is set.
+	}
+}
 
 
 int main(int argc, char** argv)
@@ -45,6 +109,12 @@ int main(int argc, char** argv)
     int portno = 0;
     int domain = AF_INET;
     char *ipstr = NULL;
+    char* myip = NULL;
+    
+    int bread;
+    int bwrote;
+    
+    char recvbuf[1024];
 
     //~ FILE* logfile;
 
@@ -70,8 +140,18 @@ int main(int argc, char** argv)
 
     int one = 1;
     int* val = &one;
+    
+    //~ int timeout = 1000;
+    //~ int* time_val = &timeout;
 
     ipstr = GetIPFromHostname(argv[1]);
+    //~ WriteLog(logfile, "Resolved IP address: ");
+	//~ WriteLogLF(logfile, ipstr);
+	
+	myip = GetMyIP();
+    //~ WriteLog(logfile, "My IP address: ");
+	//~ WriteLogLF(logfile, myip);
+	
     portno = 80;
 
     // init remote addr structure and other params
@@ -89,13 +169,21 @@ int main(int argc, char** argv)
     // Kernel, please do not fill the packet structure
     if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
     {
-        perror("setsockopt()");
+        perror("setsockopt(): cannot set IP_HDRINCL to true");
         exit(-1);
     }
+    
+    // Sets timeout
+	//~ bread = setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, time_val, sizeof(timeout));
+	//~ if (bread == -1)
+	//~ {
+		//~ perror("setsockopt(SO_SNDTIMEO)");
+		//~ SwitchErrno(errno);
+		//~ exit(-1);
+	//~ }
 
     PacketUDP PU;
-    // I don't think giving 'localhost' will work, how to get our own local IP address ?
-    ConstructUDPPacket(&PU, "127.0.0.1", ipstr);
+    ConstructUDPPacket(&PU, myip, ipstr);
 
     int ttl = 0;
 
@@ -103,17 +191,30 @@ int main(int argc, char** argv)
     {
         SetIPHeaderTTL(&(PU.iph), ttl);
 
-        if (sendto(sockfd, &PU, PU.iph.tot_len, 0, (struct sockaddr*) &server, addrlen) < 0)
+        bwrote = sendto(sockfd, &PU, PU.iph.tot_len, 0, (struct sockaddr*)&server, addrlen);
+        if (bwrote == -1)
         {
             perror("sendto()");
-            //~ exit(-1);
-        } else
+            SwitchErrno(errno);
+        }
+        else
         {
-            printf("TTL #%-2d - sendto() OK\n", ttl);
-            sleep(1);
+            printf("TTL %-2d - sendto() OK\n", ttl);
         }
 
-        /* need a recvfrom: TTL exceeded or TTL ... */
+        bread = recvfrom(sockfd, recvbuf, MAX_PACKET, 0, (struct sockaddr*)&server, &addrlen);
+        if (bread == -1)
+        {
+			perror("recvfrom()");
+			SwitchErrno(errno);
+		}
+		else
+		{
+			// a corriger en mode linux
+			printf("Message ICMP à récup\n");
+			//~ DecodeICMPHeader(recvbuf, bread, &server);
+		}
+			
     }
 
     close(sockfd);
--- src/traceroute.c
+++ src/traceroute.c
@@ -116,9 +116,11 @@ void ConstructUDPPacket(PacketUDP* buffer, const char* source, const char* dest)
 
 char *GetIPFromHostname(const char *hostname)
 {
-    struct addrinfo hints, *p, *res;
+    struct addrinfo hints, *p, *pbak, *res;
+	struct sockaddr_in *ipv4;
+    void *addr;
     char *ipstr = malloc(sizeof (char)*INET6_ADDRSTRLEN);
-    int status;
+    int status, ret;
 
     if (ipstr == NULL)
     {
@@ -136,27 +138,34 @@ char *GetIPFromHostname(const char *hostname)
         fprintf(stderr, "GetIPFromHostname(): getaddrinfo error: %s\n", gai_strerror(status));
         return NULL;
     }
-
+    
     for (p = res; p != NULL; p = p->ai_next)
     {
-        void *addr;
-
         // get the pointer to the address itself,
         // different fields in IPv4 and IPv6:
-        struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
+        ipv4 = (struct sockaddr_in *) p->ai_addr;
         addr = &(ipv4->sin_addr);
-
-        // convert the IP to a string and print it:
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-        printf("%s\n", ipstr);
-        //~ WriteLog(logfile, "Resolved IP address: ");
-        //~ WriteLogLF(logfile, ipstr);
-        break;
-    }
+        pbak = p;
+	}
+	
+	// convert the IP to a string and print it:
+	inet_ntop(pbak->ai_family, addr, ipstr, sizeof (char)*INET6_ADDRSTRLEN);
+	//~ printf("%s\n", ipstr);
 
     // get addr from command line and convert it
-    if (inet_pton(AF_INET, ipstr, ipstr) != 1)
+    if ((ret = inet_pton(AF_INET, ipstr, ipstr)) != 1)
     {
+		switch(ret)
+		{
+			case 0 :
+				fprintf(stderr, "inet_pton(): unvalid IPv4 address\n");
+				break;
+			case -1: 
+			default:
+				fprintf(stderr, "inet_pton(): unvalid address family\n");
+				break;
+		}
+        
         perror("Cannot get addr from command line and convert it");
         exit(EXIT_FAILURE);
     }
@@ -165,3 +174,140 @@ char *GetIPFromHostname(const char *hostname)
 
     return ipstr;
 }
+ 
+//~ int hostname_to_ip(char * hostname , char* ip)
+//~ {
+    //~ struct hostent *he;
+    //~ struct in_addr **addr_list;
+    //~ int i;
+         //~ 
+    //~ if ( (he = gethostbyname( hostname ) ) == NULL)
+    //~ {
+        //~ // get the host info
+        //~ herror("gethostbyname");
+        //~ return 1;
+    //~ }
+ //~ 
+    //~ addr_list = (struct in_addr **) he->h_addr_list;
+     //~ 
+    //~ for(i = 0; addr_list[i] != NULL; i++)
+    //~ {
+        //~ //Return the first one;
+        //~ strcpy(ip , inet_ntoa(*addr_list[i]) );
+        //~ return 0;
+    //~ }
+     //~ 
+    //~ return 1;
+//~ }
+
+char* GetMyIP()
+{
+	struct ifaddrs *ifaddr, *ifa;
+	int family, s;
+	char* host = (char*)malloc(128*sizeof(char));
+
+	if (getifaddrs(&ifaddr) == -1) {
+	   perror("getifaddrs");
+	   exit(EXIT_FAILURE);
+	}
+
+	/* Walk through linked list, maintaining head pointer so we
+	  can free list later */
+
+	for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+	   if (ifa->ifa_addr == NULL)
+		   continue;
+
+	   family = ifa->ifa_addr->sa_family;
+
+	   /* Display interface name and family (including symbolic
+		  form of the latter for the common families) */
+
+	   //~ printf("%s  address family: %d%s\n",
+			   //~ ifa->ifa_name, family,
+			   //~ (family == AF_PACKET) ? " (AF_PACKET)" :
+			   //~ (family == AF_INET) ?   " (AF_INET)" :
+			   //~ (family == AF_INET6) ?  " (AF_INET6)" : "");
+
+	   /* For an AF_INET* interface address, display the address */
+
+	   if (family == AF_INET || family == AF_INET6) {
+		   s = getnameinfo(ifa->ifa_addr,
+				   (family == AF_INET) ? sizeof(struct sockaddr_in) :
+										 sizeof(struct sockaddr_in6),
+				   host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+		   if (s != 0) {
+			   printf("getnameinfo() failed: %s\n", gai_strerror(s));
+			   exit(EXIT_FAILURE);
+		   }
+		   if (IsMyAddress(host) == 1)
+		   {
+				break;
+		   }
+		}
+	}
+
+	freeifaddrs(ifaddr);
+	return host;
+}
+
+int IsMyAddress(char* addr)
+{
+	if (strcmp(addr, "127.0.0.1")==0)
+	{
+		return 0;
+	}
+	
+	if (isdigit(addr[0])
+		&& isdigit(addr[1])
+		&& isdigit(addr[2])
+		&& addr[3] == '.')
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+//~ void DecodeICMPHeader(char *buf, int bytes, struct sockaddr_in *from)
+//~ {
+    //~ struct iphdr   *iphdr = NULL;
+    //~ struct icmphdr *icmphdr = NULL;
+    //~ unsigned short  iphdrlen;
+    //~ int             tick;
+    //~ static   int    icmpcount = 0;
+    //~ iphdr = (struct iphdr *)buf;
+//~ // Number of 32-bit words * 4 = bytes
+    //~ iphdrlen = iphdr->h_len * 4;
+    //~ tick = GetTickCount();
+    //~ if ((iphdrlen == MAX_IP_HDR_SIZE) && (!icmpcount))
+        //~ DecodeIPOptions(buf, bytes);
+    //~ if (bytes  < iphdrlen + ICMP_MIN)
+    //~ {
+        //~ printf("Too few bytes from %s\n",
+            //~ inet_ntoa(from->sin_addr));
+    //~ }
+    //~ icmphdr = (IcmpHeader*)(buf + iphdrlen);
+    //~ if (icmphdr->i_type != ICMP_ECHOREPLY)
+    //~ {
+        //~ printf("nonecho type %d recvd\n", icmphdr->i_type);
+        //~ return;
+    //~ }
+    //~ // Make sure this is an ICMP reply to something we sent!
+    //~ //
+    //~ if (icmphdr->i_id != (USHORT)GetCurrentProcessId())
+    //~ {
+        //~ printf("someone else's packet!\n" );
+        //~ return ;
+    //~ }
+    //~ printf("%d bytes from %s:", bytes, inet_ntoa(from->sin_addr));
+    //~ printf(" icmp_seq = %d. ", icmphdr->i_seq);
+    //~ printf(" time: %d ms", tick - icmphdr->timestamp);
+    //~ printf("\n" );
+    //~ icmpcount++;
+    //~ return;
+//~ }
+
+
+
+
