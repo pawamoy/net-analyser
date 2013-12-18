0d90f26319f8b9ca452f84eb789f4acd3878002b
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2013-12-18 12:46:39 +0100

    fused loop udp & icmp, you can use it for tcp too

--- include/traceroute.h
+++ include/traceroute.h
@@ -28,11 +28,6 @@
 #define MAX_PACKET 1024
 #define MAX_ADDRESS 128
 
-typedef struct packet_icmp {
-    struct iphdr iph;
-    struct icmphdr icmph;
-} PacketICMP;
-
 typedef struct packet_tcp {
     struct iphdr iph;
     struct tcphdr tcph;
@@ -131,27 +126,28 @@ int IsMyAddress(char* addr);
  */
 char* GetHostNameFromIP(const char* ip);
 
-/**\brief Traceroute with UDP probes
+/**\brief Traceroute
  * \param rcvt Receive timer
  * \param sndt Send timer
  * \param ttl_t Contains min_ttl, max_ttl, hops and attempts in this order
  * \param logfile FILE pointer (if NULL, don't log)
+ * \param probe Probe to use: i=icmp, t=tcp, u=udp
  * \param server Destination data
  * \param my_addr Source data
  */
-void LoopUDP(int rcvt, int sndt, int ttl_t[4], FILE* logfile,
+void LoopTrace(int rcvt, int sndt, int ttl_t[4], FILE* logfile, char probe,
              struct sockaddr_in server, struct sockaddr_in my_addr);
 
-/**\brief Traceroute with ICMP probes
- * \param rcvt Receive timer
- * \param sndt Send timer
- * \param ttl_t Contains min_ttl, max_ttl, hops and attempts in this order
- * \param logfile FILE pointer (if NULL, don't log)
- * \param server Destination data
- * \param my_addr Source data
- */
-void LoopICMP(int rcvt, int sndt, int ttl_t[4], FILE* logfile,
-             struct sockaddr_in server, struct sockaddr_in my_addr);
+//~ /**\brief Traceroute with ICMP probes
+ //~ * \param rcvt Receive timer
+ //~ * \param sndt Send timer
+ //~ * \param ttl_t Contains min_ttl, max_ttl, hops and attempts in this order
+ //~ * \param logfile FILE pointer (if NULL, don't log)
+ //~ * \param server Destination data
+ //~ * \param my_addr Source data
+ //~ */
+//~ void LoopICMP(int rcvt, int sndt, int ttl_t[4], FILE* logfile,
+             //~ struct sockaddr_in server, struct sockaddr_in my_addr);
              
 /**\brief Traceroute with TCP probes
  * \param rcvt Receive timer
