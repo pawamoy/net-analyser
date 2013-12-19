5e35e19e28002583f6983cc076d500fa2504031f
Pawamoy <gein@hotmail.fr>
2013-12-19 15:50:25 +0100

    wip main

--- src/main.c
+++ src/main.c
@@ -8,23 +8,130 @@
 #include "../include/traceroute.h"
 #include "../include/log.h" 
 
+#define DELAY   1
+#define LOSS    2
+#define FAILURE 3
+
+/* principe:
+ * ping pour vérifier la joignabilité de l'hôte
+ * première découverte de route (simple affichage + permet de récup le ttl min) + log
+ * puis on fait tourner ping (option de fréquence en ligne de commande) + log
+ * changement de TTL / variation de délai -> re-découverte de route
+ * on répète jusqu'à Ctrl-C : affichage de statistiques (voir vrai ping)
+ *     du genre % perte, délai, min max moy, routes, etc...
+ * estimation asymétrie entres routes allers et retours
+ */
+
+/* PSEUDO-CODE:
+ * 
+ * cur_ttl := 30
+ * N := 5
+ * 
+ * if HostIsJoinable ( TTL:=64 )
+ *  	begin loop
+ *  		best_ttl := traceroute ( TTL:=cur_ttl )
+ *  		if destination reached
+ *  			while ping ( TTL:=best_ttl ) ;
+ *  			case changed delay
+ *  				continue loop
+ *  			case packet loss
+ *  				for N attempts
+ *  					best_ttl++
+ *  					if ping ( TTL:=best_ttl )
+ *  						cur_ttl := best_ttl
+ *  						continue loop
+ * 						end if
+ *  				end for
+ *  				print info and exit
+ * 				end case
+ *  		else
+ *  			if HostIsJoinable ( TTL:=64 )
+ *  				print protocol problem
+ *  			end if
+ *  			print info and exit
+ *  		end if
+ * 		end loop
+ * else
+ *  	print info and exit
+ * end if
+ */
+
 int main(int argc, char* argv[]) {
-	printf("%d, %s\n", argc, *argv); // pour que ce pd de compilo fasse pas chier
+	//-----------------------------------------------------//
+	// variable declaration
+	//-----------------------------------------------------//
+	int best_ttl    = 0; // ttl minimal trouvé pour atteindre la destination
+	int cur_ttl     = 30; // ttl pour traceroute
+	int attempts    = 5; // tentatives en cas de perte pour ping (0=infini)
+	int max_ping    = 64; // ttl vérification joignabilité de l'hôte distant
+	int frequency   = 1; // fréquence inter-sonde pour ping
 	
+	//-----------------------------------------------------//
+	// first verifications
+	//-----------------------------------------------------//
 	/* analyse des arguments
 	 * séparation arg ping et arg traceroute
 	 */
 	 
-	/* principe:
-	 * ping pour vérifier la joignabilité de l'hôte
-	 * première découverte de route (simple affichage + permet de récup le ttl min) + log
-	 * puis on fait tourner ping (option de fréquence en ligne de commande) + log
-	 * changement de TTL / variation de délai -> re-découverte de route
-	 * on répète jusqu'à Ctrl-C : affichage de statistiques (voir vrai ping)
-	 *     du genre % perte, délai, min max moy, routes, etc...
-	 * estimation asymétrie entres routes allers et retours
+	//-----------------------------------------------------//
+	// start program
+	//-----------------------------------------------------//
+	//~ printf("%d, %s\n", argc, *argv); // pour que ce pd de compilo fasse pas chier
+	
+	/* STEP 1: PING WITH BIG TTL (64+)
+	 * the called function must return immediatly after joining the distant host
+	 * we should provide a number of attempt before returning failure status
+	 * no logs are written here
 	 */
-	 
+	if (HostIsJoinable(argv[1], max_ping, attempts)
+	{
+		for (;;)
+		{
+			/* STEP 2: SIMPLE TRACEROUTE WITH BIG TTL(30)
+			 * the called function should return the last TTL value, or -1 if failure
+			 */
+			best_ttl = main_traceroute(argv[1], portno, 1, cur_ttl, 1, probe,
+	                                   rcv_timeout, snd_timeout, attempts_t, 1);
+			
+			if (best_ttl == -1)
+			{
+				// destination unreached but host was joinable
+				if (HostIsJoinable(argv[1], max_ping, attempts)
+					printf("Traceroute protocol issue\n");
+				
+				ShowStatistics();
+				exit(-1);
+			}
+			else
+			{
+				/* STEP 3: launch infinite ping
+				 * the function should return:
+				 *  	DELAY when getting a variation beyond a threshold
+				 *  	LOSS if some probes are lost, but destination is reached
+				 *  		before attempts are done
+				 *  	FAILURE if some probes are lost, and
+				 *  		all attempts failed (destination unreached)
+				 */
+				switch (ping(argv[1], threshold, frequency, attempts, &best_ttl))
+				{
+					case DELAY:                       continue;
+					case LOSS:    cur_ttl = best_ttl; continue;
+					case FAILURE:
+					default:
+						ShowStatistics();
+						exit(-1);
+				}
+			}
+			/* STEP 4: loop until destination can't be reach or SIGINT
+			 */	
+		}
+	}
+	else
+	{
+		ShowStatistics();
+		exit(-1);
+	}
+	
 	
 	return 0;
 }
