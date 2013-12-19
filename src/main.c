d0a2ea02a61a62a879840c282c607781ab167a83
Pawamoy <gein@hotmail.fr>
2013-12-19 12:40:40 +0100

    principe main.c

--- src/main.c
+++ src/main.c
@@ -3,13 +3,6 @@
  * \author tim
  * \date December 18, 2013, 15:57 AM
  */
- 
-//~ - vérif avec msg ICMP request que l'hote distant est joignable, puis traceroute
-//~ - par défaut : ICMP request à intervalle régulier (opt cli)
-//~ - log des délais (ping), TTL et erreurs
-//~ - si changements (délais, erreurs) re-traceroute
-//~ - handler SIGINT : résumé statistique/graphique de toutes les infos (routes + changements, délais, pertes...)
-//~ - estimation asymétrie entres routes allers et retours
 
 #include "../include/ping.h"
 #include "../include/traceroute.h"
@@ -29,6 +22,7 @@ int main(int argc, char* argv[]) {
 	 * changement de TTL / variation de délai -> re-découverte de route
 	 * on répète jusqu'à Ctrl-C : affichage de statistiques (voir vrai ping)
 	 *     du genre % perte, délai, min max moy, routes, etc...
+	 * estimation asymétrie entres routes allers et retours
 	 */
 	 
 	
