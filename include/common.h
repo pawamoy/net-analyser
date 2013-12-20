d7fb26e9ec019b04b19735760b4b2272ac51296a
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2013-12-20 17:13:12 +0100

    ping fonctionnel, reste a ameliorer l'affichage, traiter les types de messages recus, compter les donnes

--- include/common.h
+++ include/common.h
@@ -16,8 +16,17 @@
 #include <sys/types.h>
 #include <ifaddrs.h>
 #include <ctype.h>
+#include <netinet/udp.h>
+#include <linux/icmp.h>
+#include <linux/tcp.h>
+#include <linux/ip.h>
 
 #define MAX_ADDRESS 128
+#define MAX_PACKET 1024
+
+#define ICMP_LEN 28
+#define UDP_LEN 28
+#define TCP_LEN 40
 
 typedef struct sockaddr_in Sockin;
 typedef int Socket;
@@ -49,5 +58,78 @@ int IsMyAddress(char* addr);
  * \author tim
  */
 char* GetHostNameFromIP(const char* ip);
+
+/**\brief Set TTL field (IP header) for socket 
+ * \param s socket identifier
+ * \param ttl time-to-live field (IP header)
+ * \return 0(false) on failure, 1(true) on success
+ * \author tom
+ */
+int SetTTL(Socket s, int ttl);
+
+/**\brief Set receiving timeout for socket 
+ * \param s socket identifier
+ * \param to Timeout to set
+ * \return 0(false) on failure, 1(true) on success
+ * \author tom
+ */
+int SetRCVTimeOut(Socket s, struct timeval to);
+
+/**\brief Set sending timeout for socket 
+ * \param s socket identifier
+ * \param to Timeout to set
+ * \return 0(false) on failure, 1(true) on success
+ * \author tom
+ */
+int SetSNDTimeOut(Socket s, struct timeval to);
+
+/**\brief Set HDRINCL to true (headers included) 
+ * \param s socket identifier
+ * \return 0(false) on failure, 1(true) on success
+ * \author tom
+ */
+int SetHDRINCL(Socket s);
+
+/**\brief Opens a raw socket.
+ * May print an error, and then exit with code 1
+ * \param protocol I:ICMP, U:UDP, T:TCP, default:TCP (same with lowercase)
+ * \return Socket
+ * \author tim
+ */
+Socket OpenRawSocket(char protocol);
+
+/**\brief Opens a dgram socket.
+ * May print an error, and then exit with code 1
+ * \param protocol I:ICMP, U:UDP, T:TCP, default:TCP (same with lowercase)
+ * \return Socket
+ * \author tim
+ */
+Socket OpenDgramSocket(char protocol);
+
+/**\brief Constructs an IP header
+ * \param iph Pointer to an IP Header structure
+ * \param ttl Time-to-live value
+ * \param source IP address source
+ * \param dest IP address destination
+ * \param protocol U:UDP, I:ICMP, T:TCP , default:TCP (same with lowercase)
+ * \author tim & tom
+ */
+void ConstructIPHeader(struct iphdr* iph,
+        const unsigned int ttl,
+        const char *source,
+        const char *dest,
+        const char protocol);
+
+/**\brief Constructs an ICMP header
+ * \param icmph Pointer to an ICMP header structure
+ * \author tim
+ */
+void ConstructICMPHeader(struct icmphdr* icmph);
+
+/**\brief Constructs an TCP header
+ * \param icmph Pointer to an TCP header structure
+ * \author tom
+ */
+void ConstructTCPHeader(struct tcphdr *tcph);
 
 #endif // __COMMON_H
