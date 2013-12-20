bdfbeb6c24ff7e51f160b8885abf48e632126583
Pawamoy <gein@hotmail.fr>
2013-12-20 14:44:58 +0100

    refresh report

--- include/common.h
+++ include/common.h
@@ -25,23 +25,27 @@ typedef int Socket;
 /**\brief Get IP from an hostname (ie. google.fr)
  * \param hostname String of domain name
  * \return NULL on error, a pointer to char
+ * \author tom
  */
 char* GetIPFromHostname(const char *hostname);
 
 /**\brief Get my own IP address
  * \return String (address)
+ * \author tim
  */
 char* GetMyIP(void);
 
 /**\brief Parse address to see if it is my address (not local)
  * \param addr Pointer to a string (address)
  * \return 1(yes), 0(no)
+ * \author tim
  */
 int IsMyAddress(char* addr);
 
 /**\brief Get host name from an IP address
  * \param ip The IP address
  * \return A string containing host name
+ * \author tim
  */
 char* GetHostNameFromIP(const char* ip);
 
